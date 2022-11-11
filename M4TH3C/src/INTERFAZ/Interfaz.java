55a18fcf5b64fbf6b073c6299307ed49afaa05c5
jjesusR <josedejesusramirezortega@gmail.com>
2022-11-11 01:12:15 -0600

    Cuadruplos
    Se agrego la asignacion al resultado

--- M4TH3C/src/INTERFAZ/Interfaz.java
+++ M4TH3C/src/INTERFAZ/Interfaz.java
@@ -19360,6 +19360,12 @@ public class Interfaz extends javax.swing.JFrame {
             f[3] = "T1";
             m.addRow(f);
             tblCuadruplos.setModel(m);
+            f[0] = "=";
+            f[1] = "T1";
+            f[2] = "";
+            f[3] = id1.lexemeRank(7);
+            m.addRow(f);
+            tblCuadruplos.setModel(m);
             //System.out.println(id1.lexemeRank(0)+","+id1.lexemeRank(2)+","+id1.lexemeRank(4));
         }
         
@@ -19393,7 +19399,13 @@ public class Interfaz extends javax.swing.JFrame {
             f[2] = id2.lexemeRank(6);
             f[3] = "T2";
             m.addRow(f);
-            tblCuadruplos.setModel(m);   
+            tblCuadruplos.setModel(m);
+            f[0] = "=";
+            f[1] = "T2";
+            f[2] = "";
+            f[3] = id2.lexemeRank(9);
+            m.addRow(f);
+            tblCuadruplos.setModel(m);
         }
         
         //FUNCIONES CON 4 OPERANDOS
@@ -19442,7 +19454,13 @@ public class Interfaz extends javax.swing.JFrame {
             f[2] = id2.lexemeRank(8);
             f[3] = "T3";
             m.addRow(f);
-            tblCuadruplos.setModel(m);  
+            tblCuadruplos.setModel(m);
+            f[0] = "=";
+            f[1] = "T3";
+            f[2] = "";
+            f[3] = id2.lexemeRank(11);
+            m.addRow(f);
+            tblCuadruplos.setModel(m);
         }
         //FUNCIONES CON 5 OPERANDOS
         for(Production id2: identProdF14){
@@ -19505,7 +19523,13 @@ public class Interfaz extends javax.swing.JFrame {
             f[2] = id2.lexemeRank(10);
             f[3] = "T4";
             m.addRow(f);
-            tblCuadruplos.setModel(m); 
+            tblCuadruplos.setModel(m);
+            f[0] = "=";
+            f[1] = "T4";
+            f[2] = "";
+            f[3] = id2.lexemeRank(13);
+            m.addRow(f);
+            tblCuadruplos.setModel(m);
         }
     }
     
